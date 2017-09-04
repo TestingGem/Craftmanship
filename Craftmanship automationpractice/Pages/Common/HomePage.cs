40d1b477fde9e8da7703ad4570151670fd68f9d7
Gemma Lenselink <glenselink@mirabeau.nl>
2017-09-04 11:33:12 +0200

    Added homepage url and updated packages

--- Craftmanship automationpractice/Pages/Common/HomePage.cs
+++ Craftmanship automationpractice/Pages/Common/HomePage.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
 using FluentAutomation;
-
 using Craftmanship_automationpractice.Pages;
+using Craftmanship_automationpractice.Tests;
 
 namespace Craftmanship_automationpractice.Pages.Common
 {
@@ -15,14 +14,13 @@ namespace Craftmanship_automationpractice.Pages.Common
 
         public HomePage(FluentTest test) : base(test)
         {
-            Url = "http://www.mirabeau.nl";
+            Url = ConfigurationManager.AppSettings["HomePage"];
             At = () => I.Assert.Visible(Navigation);
         }
 
         public HomePage ClickNavContactLink()
         {
             I.Click(NavContactLink);
-
             return this;
         }
     }
---
+++ Craftmanship automationpractice/Tests/AppearErrorMessage.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Craftmanship_automationpractice.Tests
+{
+    public class AppearErrorMessage : BaseTest
+    {
+        [TestCase]
+        public void NewsletterSubscription_InvalidEmail_ErrorBar()
+        {
+            new Pages.Common.HomePage(this)
+             .Go();
+        }
+    }
+}
